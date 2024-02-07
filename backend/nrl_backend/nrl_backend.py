87c37cf9c4930d71b0046cfd4c2cec4e7cf25493
Joel Hutson <joel.hutson@cmd.com.au>
2024-02-07 21:11:28 +1000

    adding dns record

--- backend/nrl_backend/nrl_backend.py
+++ backend/nrl_backend/nrl_backend.py
@@ -60,7 +60,7 @@ class FlaskFargateStack(cdk.Stack):
         )
 
         
-        dns_record = cdk.aws_route53.ARecord(
+        dns_record = cdk.aws_route53.NsRecord(
             self, "NRLRecord",
             zone=hosted_zone,
             record_name="nrl.tracker.shadbury.com",
