12e6232a6426e6f089622fcc6d1b9150314f60c4
Joel Hutson <joel.hutson@cmd.com.au>
2024-02-07 19:10:57 +1000

    Deploy Backend

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,10 @@ import os
 import aws_cdk as cdk
 from nrl_backend.nrl_backend import FlaskFargateStack, DynamodbStack
 
-dynamodb_stack = cdk.App()
-DynamodbStack(dynamodb_stack, "DynamodbStack",)
+app = cdk.App()
+DynamodbStack(app, "DynamodbStack",)
 
 fargate_stack = cdk.App()
-FlaskFargateStack(fargate_stack, "FargateStack",
+FlaskFargateStack(app, "FargateStack",
     )
-dynamodb_stack.synth()
-fargate_stack.synth()
+app.synth()
