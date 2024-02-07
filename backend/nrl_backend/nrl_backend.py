ccd6a0e4fe05d87a6d35c8af792ac6c1fa1146d9
Joel Hutson <joel.hutson@cmd.com.au>
2024-02-07 20:40:00 +1000

    testing

--- backend/nrl_backend/nrl_backend.py
+++ backend/nrl_backend/nrl_backend.py
@@ -1,14 +1,4 @@
-from aws_cdk import (
-    aws_ecs as ecs,
-    aws_ec2 as ec2,
-    aws_route53 as route53,
-    aws_route53_targets as targets,
-    aws_logs as logs,
-    aws_ecr as ecr,
-    aws_dynamodb as dynamodb,
-    aws_elasticloadbalancingv2 as elbv2,
-    Stack
-)
+from aws_cdk import cdk
 from constructs import Construct
 
 file = open("../application_whitelist/whitelist.txt", "r")
@@ -16,36 +6,36 @@ allowed_ips = file.readlines()
 file.close()
 
 
-class FlaskFargateStack(Stack):
+class FlaskFargateStack(cdk.Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        vpc = ec2.Vpc(self, "NRL_VPC", max_azs=2)
+        vpc = cdk.aws_ec2.Vpc(self, "NRL_VPC", max_azs=2)
 
-        cluster = ecs.Cluster(self, "NRL_CLUSTER", vpc=vpc)
+        cluster = cdk.aws_ecs.Cluster(self, "NRL_CLUSTER", vpc=vpc)
 
-        ecr_repo = ecr.Repository(self, "NRL_ECR_REPO")
+        ecr_repo = cdk.aws_ecr.Repository(self, "NRL_ECR_REPO")
 
-        task_definition = ecs.FargateTaskDefinition(self, "NRL_TASK_DEF")
+        task_definition = cdk.aws_ecs.FargateTaskDefinition(self, "NRL_TASK_DEF")
 
         container = task_definition.add_container(
             "NRL_CONTAINER",
-            image=ecs.ContainerImage.from_ecr_repository(ecr_repo),
+            image=cdk.aws_ecs.ContainerImage.from_ecr_repository(ecr_repo),
             memory_limit_mib=512,
-            logging=ecs.LogDriver.aws_logs(
+            logging=cdk.aws_ecs.LogDriver.aws_logs(
                 stream_prefix="NRL_APP",
-                log_retention=logs.RetentionDays.ONE_WEEK,
+                log_retention=cdk.aws_logs.RetentionDays.ONE_WEEK,
             ),
         )
 
         container.add_environment("FLASK_ENV", "production")
         container.add_environment("FLASK_APP", "app.py")
-        container.add_port_mappings(ecs.PortMapping(container_port=80))
+        container.add_port_mappings(cdk.aws_ecs.PortMapping(container_port=80))
 
         ecr_repo.grant_pull(task_definition.execution_role)
 
-        lb = elbv2.ApplicationLoadBalancer(
+        lb = cdk.aws_elbv2.ApplicationLoadBalancer(
             self, "NRL_LB",
             vpc=vpc,
             internet_facing=True,  # Set to False if you want the load balancer to be internal
@@ -54,8 +44,8 @@ class FlaskFargateStack(Stack):
 
         for ip in allowed_ips:
             lb.connections.allow_from(
-                ec2.Peer.ipv4(ip.replace("\n", "")),
-                ec2.Port.tcp(80)
+                cdk.aws_ec2.Peer.ipv4(ip.replace("\n", "")),
+                cdk.aws_ec2.Port.tcp(80)
             )
 
         listener = lb.add_listener(
@@ -64,30 +54,30 @@ class FlaskFargateStack(Stack):
             open=True
         )
 
-        hosted_zone = route53.HostedZone.from_lookup(
+        hosted_zone = cdk.aws_route53.HostedZone.from_lookup(
             self, "HostedZone",
             domain_name="shadbury.com"
         )
 
         
-        dns_record = route53.ARecord(
+        dns_record = cdk.aws_route53.ARecord(
             self, "NRLRecord",
             zone=hosted_zone,
             record_name="nrl.tracker.shadbury.com",
-            target=route53.RecordTarget.from_alias(
-                targets.LoadBalancerTarget(lb))
+            target=cdk.aws_route53.RecordTarget.from_alias(
+                cdk.aws_targets.LoadBalancerTarget(lb))
         )
         
         dns_record.node.add_dependency(lb)
 
-        fargate_service_sg = ec2.SecurityGroup(
+        fargate_service_sg = cdk.aws_ec2.SecurityGroup(
             self, "NRLServiceSG",
             vpc=vpc,
             allow_all_outbound=True
         )
-        lb.connections.allow_from(fargate_service_sg, ec2.Port.tcp(80))
+        lb.connections.allow_from(fargate_service_sg, cdk.aws_ec2.Port.tcp(80))
 
-        fargate_service = ecs.FargateService(
+        fargate_service = cdk.aws_ecs.FargateService(
             self, "NRL_SERVICE",
             cluster=cluster,
             task_definition=task_definition,
@@ -103,7 +93,7 @@ class FlaskFargateStack(Stack):
         
 
 
-class DynamodbStack(Stack):
+class DynamodbStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -119,24 +109,24 @@ class DynamodbStack(Stack):
         # Create a table for each team
         for team in teams:
             team_id = teams[team]
-            current_team = dynamodb.Table(
+            current_team = cdk.aws_dynamodb.Table(
                 self, f"{team}Table",
                 table_name=team,
-                partition_key=dynamodb.Attribute(
+                partition_key=cdk.aws_dynamodb.Attribute(
                     name="TeamName",
-                    type=dynamodb.AttributeType.STRING
+                    type=cdk.aws_dynamodb.AttributeType.STRING
                 ),
                 read_capacity=5,
                 write_capacity=5
             )
 
         # Ladder Table
-        ladder = dynamodb.Table(
+        ladder = cdk.aws_dynamodb.Table(
             self, "LadderTable",
             table_name="Ladder",
-            partition_key=dynamodb.Attribute(
+            partition_key=cdk.aws_dynamodb.Attribute(
                 name="TeamName",
-                type=dynamodb.AttributeType.STRING
+                type=cdk.aws_dynamodb.AttributeType.STRING
             ),
             read_capacity=5,
             write_capacity=5
