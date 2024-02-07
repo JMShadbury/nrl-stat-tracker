b0dbbf691d3baf245ee899fa51d33aa004ff56bb
Joel Hutson <joel.hutson@cmd.com.au>
2024-02-07 19:10:01 +1000

    Deploy Backend

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,8 @@ from nrl_backend.nrl_backend import FlaskFargateStack, DynamodbStack
 dynamodb_stack = cdk.App()
 DynamodbStack(dynamodb_stack, "DynamodbStack",)
 
-app = cdk.App()
-FlaskFargateStack(app, "FargateStack",
+fargate_stack = cdk.App()
+FlaskFargateStack(fargate_stack, "FargateStack",
     )
-
-app.synth()
 dynamodb_stack.synth()
+fargate_stack.synth()
