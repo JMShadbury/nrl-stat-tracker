aecba555dcbe24d29d47783d4286248f74f59dfd
Joel Hutson <joel.hutson@cmd.com.au>
2024-02-09 14:48:03 +1000

    format

--- backend/nrl_backend/nrl_backend.py
+++ backend/nrl_backend/nrl_backend.py
@@ -143,7 +143,7 @@ class DynamodbStack(cdk.Stack):
             read_capacity=5,
             write_capacity=5
         )
-        
+
         NRL2024Rounds = cdk.aws_dynamodb.Table(
             self, "NRL2024RoundsTable",
             table_name="NRL2024Rounds",
