4afb799a611503d8eaeeed0d8562ed2a049e67ca
Joel Shadbolt <joelshadbolt123@hotmail.com>
2024-02-29 19:39:45 +1000

    fixing pathing issue

--- scripts/scrape/update_teams.py
+++ scripts/scrape/update_teams.py
@@ -47,7 +47,7 @@ try:
     }
 
     logger.info("Updating teams data")
-    with open("scripts/app/data/teams", "r") as f:
+    with open("scripts/app/teams/teams", "r") as f:
         team_names = f.read().splitlines()
 
     logger.debug(f"Teams: {team_names}")
