e6f6354f0bbfc9aeccf2770e625f179f87127989
Joel Shadbolt <joelshadbolt123@hotmail.com>
2024-03-12 18:56:55 +1000

    pylint

--- scrape/stats/get_stats.py
+++ scrape/stats/get_stats.py
@@ -95,4 +95,3 @@ class Stats:
                     if team_name_element and team_name_element.get_text(strip=True).replace(" ", "") == team_name:
                         return process_table_row(row, self.stat)
         return None
-
